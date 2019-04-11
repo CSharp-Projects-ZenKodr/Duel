56cd85be7bccfcd03cad19f6bde1d95374f0d79c
Habab <hi04031@st.habib.edu.pk>
2019-04-12 00:55:41 +0500

    Create TilesClicked.cs

---
+++ WW/Assets/Scripts/newGameScreen/TilesClicked.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesClicked : MonoBehaviour
+{
+    public GameObject TileInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TileInfo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void checkTileInfo()
+    {
+        TileInfo.SetActive(!TileInfo.activeSelf);
+    }
+}
