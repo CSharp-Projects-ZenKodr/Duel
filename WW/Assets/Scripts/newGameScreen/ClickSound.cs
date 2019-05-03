50e12053f2a13a3b80464a5e30eba9ef9a7f6064
Habab <hi04031@st.habib.edu.pk>
2019-05-03 10:17:44 +0500

    Create ClickSound.cs

---
+++ WW/Assets/Scripts/newGameScreen/ClickSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+//[RequireComponent(typeof(Button))]
+
+public class ClickSound : MonoBehaviour
+{
+    public AudioClip sound;
+    public AudioSource source;
+    public void PlaySound()
+    {
+        source.PlayOneShot(sound);
+    }
+}
