c40d4a860f48bcdd3bda25c0d93f6a774c091c23
Muhammad Shahrom Ali <estineali@gmail.com>
2019-12-15 21:26:49 +0500

    Restructure

---
+++ Assets/Scripts/newGameScreen/EditText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditText : MonoBehaviour
+{
+    public Text countText;
+    public int timer;
+
+    public IEnumerator countDown()
+    { //Called by gamestate.DelayEnumerator
+
+        Controller.TurnChanging = true;
+        int timer_local = timer;
+
+        for (int i = 0; i < timer_local; i++)
+        {
+            countText.text = (timer_local - i).ToString();
+            yield return new WaitForSeconds(1);
+        }
+
+        this.gameObject.SetActive(false);
+        Controller.TurnChanging = false;
+    }
+
+    public void setTimer(int timerDuration)
+    {
+        timer = timerDuration;
+    }
+}
